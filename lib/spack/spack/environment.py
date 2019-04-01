0269d9fc3b0a46d1b9af12d504aa5c539fedecec
Gregory Becker <becker33@llnl.gov>
2019-04-01 11:07:09 -0700

    python3 compatibility fixes

--- lib/spack/spack/environment.py
+++ lib/spack/spack/environment.py
@@ -723,7 +723,7 @@ class Environment(object):
 
             elif removed:
                 new_reference = dict((n, self.read_specs[n])
-                                     for n in self.read_specs.keys()[:i])
+                                     for n in list(self.read_specs.keys())[:i])
                 speclist.update_reference(new_reference)
 
     def concretize(self, force=False):
